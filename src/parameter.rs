4a4a8919b4d5badee92318aeab01c6422a836258
Sergei Beljakov <1899416+tasmanianfox@users.noreply.github.com>
2020-09-07 20:38:58 +0300

    Replaced some values to unions

--- src/parameter.rs
+++ src/parameter.rs
@@ -10,11 +10,15 @@ enum ParameterType {
     Struct
 }
 
-#[allow(dead_code)]
-pub struct Parameter {
+union Value {
     boolean_value: bool,
     float_value: f64,
     int_value: i128,
+}
+
+#[allow(dead_code)]
+pub struct Parameter {
+    value: Value,
     string_value: String,
     struct_value: ParameterStore,
     param_type: ParameterType
@@ -23,10 +27,8 @@ pub struct Parameter {
 impl Parameter {
     pub fn new() -> Parameter {
         Parameter {
-            boolean_value: false,
+            value: Value { int_value: 0 },
             string_value: String::new(),
-            int_value: 0,
-            float_value: 0.0,
             param_type: ParameterType::StringType,
             struct_value: HashMap::new(),
         }
@@ -34,17 +36,17 @@ impl Parameter {
 
     pub fn set_boolean_value(&mut self, value: bool) {
         self.param_type = ParameterType::Boolean;
-        self.boolean_value = value;
+        self.value = Value { boolean_value: value };
     }
 
     pub fn set_float_value(&mut self, value: f64) {
         self.param_type = ParameterType::Float;
-        self.float_value = value;
+        self.value = Value { float_value: value };
     }
 
     pub fn set_int_value(&mut self, value: i128) {
         self.param_type = ParameterType::Int;
-        self.int_value = value;
+        self.value = Value { int_value: value };
     }
 
     pub fn set_string_value(&mut self, value: String) {
@@ -58,12 +60,14 @@ impl Parameter {
     }
 
     pub fn get_string_value(&self) -> String {
-        match self.param_type {
-            ParameterType::Boolean => String::from(if self.boolean_value == true { "true" } else { "false" }),
-            ParameterType::Float => self.float_value.to_string(),
-            ParameterType::Int => self.int_value.to_string(),
-            ParameterType::StringType => self.string_value.clone(),
-            ParameterType::Struct => String::new() // TODO: implement
+        unsafe {
+            match self.param_type {
+                ParameterType::Boolean => String::from(if self.value.boolean_value == true { "true" } else { "false" }),
+                ParameterType::Float => self.value.float_value.to_string(),
+                ParameterType::Int => self.value.int_value.to_string(),
+                ParameterType::StringType => self.string_value.clone(),
+                ParameterType::Struct => String::new() // TODO: implement
+            }
         }
     }
 }
